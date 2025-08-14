2c537bc917666c5a71a46ad7454239060b1cdb67
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-08-14 12:06:50 -0400

    IMplemented summary cards

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -4,13 +4,19 @@ import { useUserAuth } from '../../hooks/useUserAuth';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_Paths } from '../../utils/apiPaths';
+import InfoCard from '../../components/Cards/InfoCard';
+
+import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
+import { IoMdCard } from "react-icons/io"
+import { addThousandsSeperator } from '../../utils/helper';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 const Home = () => {
     useUserAuth();
 
     const navigate = useNavigate();
 
-    const [DashboardData, setDashboardData] = useState(null);
+    const [dashboardData, setDashboardData] = useState(null);
     const [loading, setLoading] = useState(false)
 
     const fetchDashboard = async () => {
@@ -41,7 +47,36 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className='my-5 mx-auto'>
-        
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeperator(dashboardData?.totalBalance || 0 )}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThousandsSeperator(dashboardData?.totalIncome || 0 )}
+            color="bg-orange-500"
+          />
+
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={addThousandsSeperator(dashboardData?.totalExpense || 0 )}
+            color="bg-red-500"
+          />
+
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+          <RecentTransactions
+            transactions={dashboardData?.RecentTransactions}
+            onSeeMore={() => navigate("/expense")}
+          />
+        </div>
       </div>
     </DashboardLayout>
   )
