da63e060c1bf10c18ac821be805be064e8ece617
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-08-19 15:10:29 -0400

    Changed the base_url to my render url

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://expense-tracker-application-8b0i.onrender.com";
 
 // utils/apiPaths.js
 export const API_Paths = {
